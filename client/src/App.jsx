f7e24b647f6a1c29b3e7b1318a48e188be36044a
siroratN <118073607+siroratN@users.noreply.github.com>
2024-08-17 15:33:59 +0700

    ,

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from 'react';
+import Register from '../src/pages/register'
+import Login from '../src/pages/login'
 
 export default function App() {
   return (
-    <div className='h-screen bg-black'>
-      d
+    <div className="h-[2000px] w-screen bg-[#242423] flex">
+      <div className="w-full">
+        <div>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="register/" element={<Register />} />
+          </Routes>
+        </div>
+      </div>
     </div>
-  )
+  );
 }

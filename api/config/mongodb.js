92f793d79ca9e10654020d19665a118f241cf126
DefinitelyNotJay <jjbrathers3@gmail.com>
2024-10-26 19:25:41 +0700

    Update mongodb.js

--- api/config/mongodb.js
+++ api/config/mongodb.js
@@ -1,23 +1,3 @@
-// import mongoose from "mongoose";
-
-// let cachedDb = null;
-
-// export const connectToDatabase = async () => {
-//   if (cachedDb) {
-//     console.log('=> Using existing database connection');
-//     return cachedDb;
-//   }
-
-//   console.log('=> Creating new database connection');
-//   const db = await mongoose.connect(process.env.MONGO_URL, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   });
-
-//   cachedDb = db;
-//   return cachedDb;
-// };
-
 import mongoose from 'mongoose';
 
 export default async function connectDB() {

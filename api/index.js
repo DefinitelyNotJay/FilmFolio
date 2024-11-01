250c30d10abba3ddc3a7afef8954a07cc38c2c77
DefinitelyNotJay <jjbrathers3@gmail.com>
2024-11-01 18:35:43 +0700

    My commit message

--- api/index.js
+++ api/index.js
@@ -1,33 +1,36 @@
-import express from "express";
-import dotenv from "dotenv";
-import movieRoute from "./routes/movieRoute.js";
-import commentRoute from "./routes/commentRoute.js";
-import serverless from "serverless-http";
-import cors from "cors";
-import mongoose from "mongoose";
+import express from 'express';
+import dotenv from 'dotenv';
+import movieRoute from './routes/movieRoute.js';
+import commentRoute from './routes/commentRoute.js';
+import serverless from 'serverless-http';
+import cors from 'cors';
+import mongoose from 'mongoose';
 
-import { Comment, Movie, User } from "./model/Model.js";
+import { Comment, Movie, User } from './model/Model.js';
 
 const app = express();
 dotenv.config();
 
 app.use(express.json());
-app.use(cors());
+app.use(
+	cors({
+		origin: 'http://54.80.58.214',
+	})
+);
 
-app.get("/", async (req, res, next) => {
-  const comments = await User.find();
-  res.status(200).json(comments);
+app.get('/', async (req, res, next) => {
+	const comments = await User.find();
+	res.status(200).json(comments);
 });
 
-app.use("/api/movie", movieRoute);
-app.use("/api/comment", commentRoute);
-
+app.use('/api/movie', movieRoute);
+app.use('/api/comment', commentRoute);
 
 async function connectDB() {
 	if (mongoose.connection.readyState === 0) {
 		try {
 			await mongoose.connect(process.env.MONGO_URL);
-      console.log("db success")
+			console.log('db success');
 		} catch (error) {
 			console.error('Error connecting to MongoDB:', error);
 			throw error;
@@ -35,22 +38,21 @@ async function connectDB() {
 	}
 }
 
-
-app.listen("3000", async () => {
-  await connectDB()
-  console.log(`server start on port ${process.env.PORT}`);
-  console.log("Query", await Movie.find())
+app.listen('3000', async () => {
+	await connectDB();
+	console.log(`server start on port ${process.env.PORT}`);
+	console.log('Query', await Movie.find());
 });
 
 // Export Lambda Handler
 export const handler = async (event, context) => {
-  context.callbackWaitsForEmptyEventLoop = false;
-  // เชื่อมต่อกับฐานข้อมูลก่อนที่จะสร้าง server
-  await connectDB()
+	context.callbackWaitsForEmptyEventLoop = false;
+	// เชื่อมต่อกับฐานข้อมูลก่อนที่จะสร้าง server
+	await connectDB();
 
-  // ใช้ serverless เพื่อจัดการ Express App
-  const serverlessApp = serverless(app);
-  
-  // ส่ง event และ context ให้ Lambda handler ของ serverless
-  return serverlessApp(event, context);
+	// ใช้ serverless เพื่อจัดการ Express App
+	const serverlessApp = serverless(app);
+
+	// ส่ง event และ context ให้ Lambda handler ของ serverless
+	return serverlessApp(event, context);
 };

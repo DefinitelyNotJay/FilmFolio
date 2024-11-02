9d3b5285ae8224bc748e57310dfa0bbfc90345e4
DefinitelyNotJay <jjbrathers3@gmail.com>
2024-11-03 01:00:37 +0700

    d

--- client/src/pages/user/UserLayout.jsx
+++ client/src/pages/user/UserLayout.jsx
@@ -1,10 +1,15 @@
 import { Outlet } from 'react-router-dom';
 import UserFooterBar from '@/components/UserFooterBar';
+import UserTopBar from '@/components/UserTopBar';
+import { useParams } from 'react-router-dom';
 export default function UserLayout() {
-  return (
-    <div className="w-[391px] h-screen bg-[#201c1c]  font-Inter">
-      < UserFooterBar/>
-      <Outlet /> {/* Renders the matched child route */}
-    </div>
-  );
+  const params = useParams()
+  console.log(params)
+	return (
+		<div className="w-[391px] h-screen bg-[#201c1c] font-Inter">
+			<UserTopBar />
+			<UserFooterBar />
+			<Outlet /> {/* Renders the matched child route */}
+		</div>
+	);
 }

18bc069d45d210aa9be85a880d8224063dbbe81c
DefinitelyNotJay <jjbrathers3@gmail.com>
2024-11-02 16:06:20 +0700

    initial design

--- client/src/pages/user/MovieDetail.jsx
+++ client/src/pages/user/MovieDetail.jsx
@@ -1,14 +1,56 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
+import { useParams } from 'react-router-dom';
+import { ThumbsUp, ThumbsDown, CircleUserRound } from 'lucide-react';
+import { Button } from '@/components/ui/button';
 
 const MovieDetail = () => {
-    const {id} = useParams()
-    console.log(id)
-  return (
-    <div>
-        detail {id}
-    </div>
-  )
-}
+	const { id } = useParams();
+	console.log(id);
+	return (
+		<div className="text-[#fdfdff] p-12 h-full">
+			<section>
+				<img src="/panda.jpg" alt="" className='h-60 mx-auto rounded-md' />
+				<div className="text-center flex flex-col gap-1 mt-2">
+					<h1 className="text-2xl font-semibold">Kungfu</h1>
+					<p className="text-sm">DreamWorks Animation</p>
+					<p className="text-sm">★ ★ ★ ★ ☆</p>
+				</div>
+			</section>
+			<section className="mt-4 flex flex-col gap-3">
+				<div>
+					<h1 className="font-semibold">• Movie Subject</h1>
+					<p className='text-sm'>
+						Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad molestiae fugiat,
+						accusantium sint culpa velit voluptatem repellendus veritatis blanditiis! Cumque iste
+						dolorem obcaecati quis deserunt esse officiis id qui assumenda.
+					</p>
+				</div>
+				<div>
+					<div className="flex justify-between items-center mb-4">
+						<h1 className="font-semibold">• User review</h1>
+						<Button className="bg-[#E49600] text-[12px] rounded-md">COMMENT</Button>
+					</div>
+					<div className="p-3 border flex flex-col gap-2 rounded-xl bg-[#333] mt-1">
+						<div className="icon flex gap-2 items-center">
+							<CircleUserRound size={32} />
+							<p>ป้อเองนักเลงพอ</p>
+						</div>
+						<p className="leading-7">
+							Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad molestiae fugiat,
+							accusantium sint culpa velit voluptatem repellendus veritatis blanditiis! Cumque iste
+							dolorem obcaecati quis deserunt esse officiis id qui assumenda.
+						</p>
+						<div className="flex items-center justify-between">
+							<p>2 likes</p>
+							<div className="flex gap-2">
+								<ThumbsUp />
+								<ThumbsDown />
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+		</div>
+	);
+};
 
-export default MovieDetail
+export default MovieDetail;
--- client/src/pages/user/UserLayout.jsx
+++ client/src/pages/user/UserLayout.jsx
@@ -1,10 +1,10 @@
-import { Outlet } from "react-router-dom";
-import UserFooterBar from "@/components/UserFooterBar";
+import { Outlet } from 'react-router-dom';
+import UserFooterBar from '@/components/UserFooterBar';
 export default function UserLayout() {
-  return (
-    <div className="w-[390px] h-screen  bg-[#282424]  font-Inter">
-      < UserFooterBar/>
-      <Outlet /> {/* Renders the matched child route */}
-    </div>
-  );
+	return (
+		<div className="w-[390px] h-screen  bg-[#282424]  font-Inter">
+			<UserFooterBar />
+			<Outlet /> {/* Renders the matched child route */}
+		</div>
+	);
 }

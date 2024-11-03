e643c48ab070395e02a9d20cf57a5356d0412168
DefinitelyNotJay <jjbrathers3@gmail.com>
2024-11-03 12:16:10 +0700

    config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,13 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-})
+	plugins: [react()],
+	server: {
+		proxy: {
+			'/api': {
+				target: 'http://3.81.23.137:3000', // URL ของ Backend
+				changeOrigin: true, // เปลี่ยน Origin ของคำขอ
+				rewrite: (path) => path.replace(/^\/api/, ''), // ปรับเส้นทางถ้าจำเป็น
+			},
+		},
+	},
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './src'),
+		},
+	},
+});

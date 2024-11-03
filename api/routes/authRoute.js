88605c185a3603cb633b369518e5c5b916b715b5
DefinitelyNotJay <jjbrathers3@gmail.com>
2024-11-03 13:01:29 +0700

    d

--- api/routes/authRoute.js
+++ api/routes/authRoute.js
@@ -5,10 +5,6 @@ import { loginUser, registerUser } from '../controllers/authController.js'
 
 
 const router = express.Router()
-router.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true
-}));
 router.post("/register", registerUser)
 router.post("/login", loginUser)
 
